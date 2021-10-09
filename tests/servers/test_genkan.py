c26a26ea7031130fa21779d3e6a57ff2a97b4e7a
Valéry Febvre <vfebvre@easter-eggs.com>
2021-10-10 00:08:03 +0200

    [Tests] Genkan: Update

--- tests/servers/test_genkan.py
+++ tests/servers/test_genkan.py
@@ -9,63 +9,42 @@ logging.basicConfig(level=logging.DEBUG)
 
 @pytest.fixture
 def edelgardescans_server():
-    from komikku.servers.genkan import Edelgardescans
+    from komikku.servers.edelgardescans import Edelgardescans
 
     return Edelgardescans()
 
 
 @pytest.fixture
 def hatigarmscans_server():
-    from komikku.servers.genkan import Hatigarmscans
+    from komikku.servers.hatigarmscans import Hatigarmscans
 
     return Hatigarmscans()
 
 
 @pytest.fixture
 def hunlightscans_server():
-    from komikku.servers.genkan import Hunlightscans
+    from komikku.servers.hunlightscans import Hunlightscans
 
     return Hunlightscans()
 
 
 @pytest.fixture
-def leviatanscans_server():
-    from komikku.servers.genkan import Leviatanscans
-
-    return Leviatanscans()
-
-
-@pytest.fixture
-def leviatanscans_es_server():
-    from komikku.servers.genkan import Leviatanscans_es
-
-    return Leviatanscans_es()
-
-
-@pytest.fixture
-def oneshotscans_server():
-    from komikku.servers.genkan import Oneshotscans
-
-    return Oneshotscans()
-
-
-@pytest.fixture
 def reaperscans_server():
-    from komikku.servers.genkan import Reaperscans
+    from komikku.servers.reaperscans import Reaperscans
 
     return Reaperscans()
 
 
 @pytest.fixture
 def thenonamesscans_server():
-    from komikku.servers.genkan import Thenonamesscans
+    from komikku.servers.thenonamesscans import Thenonamesscans
 
     return Thenonamesscans()
 
 
 @pytest.fixture
 def zeroscans_server():
-    from komikku.servers.genkan import Zeroscans
+    from komikku.servers.zeroscans import Zeroscans
 
     return Zeroscans()
 
@@ -245,211 +224,6 @@ def test_hunlightscans(hunlightscans_server):
     print('Get page image')
     try:
         response = hunlightscans_server.get_manga_chapter_page_image(None, None, None, page)
-    except Exception as e:
-        response = None
-        log_error_traceback(e)
-
-    assert response is not None
-    yield
-
-
-@test_steps('get_most_popular', 'search', 'get_manga_data', 'get_chapter_data', 'get_page_image')
-def test_leviatanscans(leviatanscans_server):
-    # Server is disabled
-    yield
-    yield
-    yield
-    yield
-    yield
-
-    # Get most popular
-    print('Get most popular')
-    try:
-        response = leviatanscans_server.get_most_populars()
-    except Exception as e:
-        response = None
-        log_error_traceback(e)
-
-    assert response is not None
-    yield
-
-    # Search
-    print('Search')
-    try:
-        response = leviatanscans_server.search('auto hunting')
-        slug = response[0]['slug']
-    except Exception as e:
-        slug = None
-        log_error_traceback(e)
-
-    assert slug is not None
-    yield
-
-    # Get manga data
-    print('Get manga data')
-    try:
-        response = leviatanscans_server.get_manga_data(dict(slug=slug))
-        chapter_slug = response['chapters'][0]['slug']
-    except Exception as e:
-        chapter_slug = None
-        log_error_traceback(e)
-
-    assert chapter_slug is not None
-    yield
-
-    # Get chapter data
-    print("Get chapter data")
-    try:
-        response = leviatanscans_server.get_manga_chapter_data(slug, None, chapter_slug, None)
-        page = response['pages'][0]
-    except Exception as e:
-        page = None
-        log_error_traceback(e)
-
-    assert page is not None
-    yield
-
-    # Get page image
-    print('Get page image')
-    try:
-        response = leviatanscans_server.get_manga_chapter_page_image(None, None, None, page)
-    except Exception as e:
-        response = None
-        log_error_traceback(e)
-
-    assert response is not None
-    yield
-
-
-@test_steps('get_most_popular', 'search', 'get_manga_data', 'get_chapter_data', 'get_page_image')
-def test_leviatanscans_es(leviatanscans_es_server):
-    # Server is disabled
-    yield
-    yield
-    yield
-    yield
-    yield
-
-    # Get most popular
-    print('Get most popular')
-    try:
-        response = leviatanscans_es_server.get_most_populars()
-    except Exception as e:
-        response = None
-        log_error_traceback(e)
-
-    assert response is not None
-    yield
-
-    # Search
-    print('Search')
-    try:
-        response = leviatanscans_es_server.search('solo leveling')
-        slug = response[0]['slug']
-    except Exception as e:
-        slug = None
-        log_error_traceback(e)
-
-    assert slug is not None
-    yield
-
-    # Get manga data
-    print('Get manga data')
-    try:
-        response = leviatanscans_es_server.get_manga_data(dict(slug=slug))
-        chapter_slug = response['chapters'][0]['slug']
-    except Exception as e:
-        chapter_slug = None
-        log_error_traceback(e)
-
-    assert chapter_slug is not None
-    yield
-
-    # Get chapter data
-    print("Get chapter data")
-    try:
-        response = leviatanscans_es_server.get_manga_chapter_data(slug, None, chapter_slug, None)
-        page = response['pages'][0]
-    except Exception as e:
-        page = None
-        log_error_traceback(e)
-
-    assert page is not None
-    yield
-
-    # Get page image
-    print('Get page image')
-    try:
-        response = leviatanscans_es_server.get_manga_chapter_page_image(None, None, None, page)
-    except Exception as e:
-        response = None
-        log_error_traceback(e)
-
-    assert response is not None
-    yield
-
-
-@test_steps('get_most_popular', 'search', 'get_manga_data', 'get_chapter_data', 'get_page_image')
-def test_oneshotscans(oneshotscans_server):
-    # Server is disabled
-    yield
-    yield
-    yield
-    yield
-    yield
-
-
-    # Get most popular
-    print('Get most popular')
-    try:
-        response = oneshotscans_server.get_most_populars()
-    except Exception as e:
-        response = None
-        log_error_traceback(e)
-
-    assert response is not None
-    yield
-
-    # Search
-    print('Search')
-    try:
-        response = oneshotscans_server.search('the distinguished cute master')
-        slug = response[0]['slug']
-    except Exception as e:
-        slug = None
-        log_error_traceback(e)
-
-    assert slug is not None
-    yield
-
-    # Get manga data
-    print('Get manga data')
-    try:
-        response = oneshotscans_server.get_manga_data(dict(slug=slug))
-        chapter_slug = response['chapters'][0]['slug']
-    except Exception as e:
-        chapter_slug = None
-        log_error_traceback(e)
-
-    assert chapter_slug is not None
-    yield
-
-    # Get chapter data
-    print("Get chapter data")
-    try:
-        response = oneshotscans_server.get_manga_chapter_data(slug, None, chapter_slug, None)
-        page = response['pages'][0]
-    except Exception as e:
-        page = None
-        log_error_traceback(e)
-
-    assert page is not None
-    yield
-
-    # Get page image
-    print('Get page image')
-    try:
-        response = oneshotscans_server.get_manga_chapter_page_image(None, None, None, page)
     except Exception as e:
         response = None
         log_error_traceback(e)
