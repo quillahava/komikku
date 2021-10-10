ea5a01564b40124e4429cc6cd5c5313a4df9ff19
Valéry Febvre <vfebvre@easter-eggs.com>
2021-10-10 23:12:17 +0200

    [Servers] TuttoAnimeManga: Disable
    Still alive, but new site use an old version of MangaDex

--- komikku/servers/tuttoanimemanga/__init__.py
+++ komikku/servers/tuttoanimemanga/__init__.py
@@ -9,11 +9,12 @@ from komikku.servers.multi.foolslide import FoOlSlide
 
 class Tuttoanimemanga(FoOlSlide):
     id = 'tuttoanimemanga'
-    name = 'Tutto Anime Manga'
+    name = 'Tutto Anime Manga (TAM)'
     lang = 'it'
+    status = 'disabled'  # Still alive, but new site use an old version of MangaDex
 
-    base_url = 'http://tuttoanimemanga.net/slide'
+    base_url = 'http://tuttoanimemanga.net'
     search_url = base_url + '/search'
     mangas_url = base_url + '/directory'
-    manga_url = base_url + '/series/{0}'
+    manga_url = base_url + '/comics/{0}'
     chapter_url = base_url + '/read/{0}/it/{1}/page/1'
