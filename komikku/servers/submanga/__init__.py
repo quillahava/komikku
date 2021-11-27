b3e0d4f3ff991fb3eb64aa823bfe84d692d2ac71
Valéry Febvre <vfebvre@easter-eggs.com>
2021-11-27 01:45:16 +0100

    [Servers] Submanga: Update
    Switch to Madara (multi)

--- komikku/servers/submanga/__init__.py
+++ komikku/servers/submanga/__init__.py
@@ -4,18 +4,13 @@
 # SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
-from komikku.servers.multi.my_manga_reader_cms import MyMangaReaderCMSv1
+from komikku.servers.multi.madara import Madara
 
 
-class Submanga(MyMangaReaderCMSv1):
+class Submanga(Madara):
     id = 'submanga'
     name = 'Submanga'
     lang = 'es'
 
     base_url = 'https://submanga.io'
-    search_url = base_url + '/search'
-    most_populars_url = base_url + '/filterList?page=1&sortBy=views&asc=false'
-    manga_url = base_url + '/manga/{0}'
-    chapter_url = base_url + '/manga/{0}/{1}'
-    image_url = base_url + '/uploads/manga/{0}/chapters/{1}/{2}'
-    cover_url = base_url + '/uploads/manga/{0}/cover/cover_250x350.jpg'
+    chapters_url = base_url + '/manga/{0}/ajax/chapters/'
