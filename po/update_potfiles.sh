1d1f4bb350d610a154751930b568f0b56d78cb52
Valéry Febvre <vfebvre@easter-eggs.com>
2022-09-02 01:07:37 +0200

    [L10n] Sync all .po files
    Update French translation

--- po/update_potfiles.sh
+++ po/update_potfiles.sh
@@ -5,7 +5,7 @@ version=$(fgrep "version: " ../meson.build | grep -v "meson" | grep -o "'.*'" | 
 find ../komikku -iname "*.py" | xargs xgettext --package-name=Komikku --package-version=$version --from-code=UTF-8 --output=komikku-python.pot
 find ../data/ui -iname "*.ui" -or -iname "*.xml" -or -iname "*.ui.in" | xargs xgettext --package-name=Komikku --package-version=$version --from-code=UTF-8 --output=komikku-glade.pot -L Glade
 find ../data/ -iname "*.desktop.in" | xargs xgettext --package-name=Komikku --package-version=$version --from-code=UTF-8 --output=komikku-desktop.pot -L Desktop
-find ../data/ -iname "*.appdata.xml.in" | xargs xgettext --no-wrap --package-name=Komikku --package-version=$version --from-code=UTF-8 --output=komikku-appdata.pot
+find ../data/ -iname "*.appdata.xml.in.in" | xargs xgettext --no-wrap --package-name=Komikku --package-version=$version --from-code=UTF-8 --output=komikku-appdata.pot
 
 msgcat --sort-by-file --use-first --output-file=komikku.pot komikku-python.pot komikku-glade.pot komikku-desktop.pot komikku-appdata.pot
 
