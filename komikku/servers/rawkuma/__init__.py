73553ba101382ff8db0deb461b195f97a3974402
Valéry Febvre <vfebvre@easter-eggs.com>
2022-09-01 10:37:33 +0200

    [Servers] Rawkuma: Update

--- komikku/servers/rawkuma/__init__.py
+++ komikku/servers/rawkuma/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (C) 2019-2021 Valéry Febvre
+# Copyright (C) 2019-2022 Valéry Febvre
 # SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
@@ -15,12 +15,12 @@ class Rawkuma(MangaStream):
     base_url = 'https://rawkuma.com'
     search_url = base_url + '/manga/'
     manga_url = base_url + '/manga/{0}/'
-    chapter_url = base_url + '/{0}-chapter-{1}/'
+    chapter_url = base_url + '/{1}/'  # manga slug is not used
 
     name_selector = '.entry-title'
     thumbnail_selector = '.thumb img'
-    authors_selector = '.infox .fmed:contains("Artist") span, .infox .fmed:contains("Author") span'
+    authors_selector = '.infox .fmed:-soup-contains("Artist") span, .infox .fmed:-soup-contains("Author") span'
     genres_selector = '.infox .mgen a'
-    scanlators_selector = '.infox .fmed:contains("Serialization") span'
-    status_selector = '.tsinfo .imptdt:contains("Status") i'
+    scanlators_selector = '.infox .fmed:-soup-contains("Serialization") span'
+    status_selector = '.tsinfo .imptdt:-soup-contains("Status") i'
     synopsis_selector = '[itemprop="description"] p'
