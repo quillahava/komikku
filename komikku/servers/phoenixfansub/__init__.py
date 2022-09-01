e6569825df88a22519e9d856d429c6a8cee2513c
Valéry Febvre <vfebvre@easter-eggs.com>
2022-09-01 10:37:04 +0200

    [Servers] Phoenix Fansub: Update

--- komikku/servers/phoenixfansub/__init__.py
+++ komikku/servers/phoenixfansub/__init__.py
@@ -15,12 +15,12 @@ class Phoenixfansub(MangaStream):
     base_url = 'https://phoenixfansub.com'
     search_url = base_url + '/manga/'
     manga_url = base_url + '/manga/{0}/'
-    chapter_url = base_url + '/{0}-capitulo-{1}/'
+    chapter_url = base_url + '/{1}/'  # manga slug is not used
 
     name_selector = '.entry-title'
     thumbnail_selector = '.thumb img'
-    authors_selector = '.tsinfo.bixbox .imptdt:contains("Artist") i, .tsinfo.bixbox .imptdt:contains("Author") i'
+    authors_selector = '.tsinfo.bixbox .imptdt:-soup-contains("Artist") i, .tsinfo.bixbox .imptdt:-soup-contains("Author") i'
     genres_selector = '.info-right .mgen a'
-    scanlators_selector = '.tsinfo.bixbox .imptdt:contains("Serialization") i'
-    status_selector = '.tsinfo.bixbox .imptdt:contains("Status") i'
+    scanlators_selector = '.tsinfo.bixbox .imptdt:-soup-contains("Serialization") i'
+    status_selector = '.tsinfo.bixbox .imptdt:-soup-contains("Status") i'
     synopsis_selector = '[itemprop="description"] p'
