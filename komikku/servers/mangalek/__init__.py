38a0680a04897a7fd6e4afe07dcb8f9104c87bd7
Valéry Febvre <vfebvre@easter-eggs.com>
2023-01-04 01:51:32 +0100

    [Servers] Adds Mangalek [AR]

---
+++ komikku/servers/mangalek/__init__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019-2023 Valéry Febvre
+# SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
+# Author: Valéry Febvre <vfebvre@easter-eggs.com>
+
+from komikku.servers.multi.madara import Madara
+
+
+class Mangalek(Madara):
+    id = 'mangalek'
+    name = 'مانجا ليك Mangalek'
+    lang = 'ar'
+
+    date_format = '%Y ,%d %B'
+
+    base_url = 'https://mangalek.net'
+    chapter_url = base_url + '/manga/{0}/{1}/'
