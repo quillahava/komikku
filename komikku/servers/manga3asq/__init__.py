dd205e0561cb72514c8a4eb733fa1661f2759d6a
Valéry Febvre <vfebvre@easter-eggs.com>
2023-04-27 07:48:52 +0200

    [Servers] 3asq: Update

--- komikku/servers/manga3asq/__init__.py
+++ komikku/servers/manga3asq/__init__.py
@@ -2,10 +2,10 @@
 # SPDX-License-Identifier: GPL-3.0-only or GPL-3.0-or-later
 # Author: Valéry Febvre <vfebvre@easter-eggs.com>
 
-from komikku.servers.multi.madara import Madara
+from komikku.servers.multi.madara import Madara2
 
 
-class Manga3asq(Madara):
+class Manga3asq(Madara2):
     id = 'manga3asq'
     name = 'مانجا العاشق'
     lang = 'ar'
